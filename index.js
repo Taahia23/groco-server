9680b64bd2b31539e9f6d670135c5ecc8fc93ef3
Taahia23 <taahiatahsin@gmail.com>
2024-01-10 02:42:10 +0600

    connected with mongodb

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g9rirok.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const foodItemCollection = client.db("grocoDB").collection("foodItem");
+
+    app.get("/foodItem", async(req, res) => {
+        const result = await foodItemCollection.find().toArray();
+        res.send(result)
+    })
+
+    // for detail food route
+    app.get('/foodItem/:id' , async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await foodItemCollection.findOne(query);
+      res.send(result)
+    })
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('Groco is running');
+});
+
+app.listen(port, () => {
+    console.log(`Groco server is running on port ${port}`);
+})
